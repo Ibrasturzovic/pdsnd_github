149fb16ed18d6cf881dbf0b3b979f06ddd8cde6e
ibrasturzovic <85612313+ibrasturzovic@users.noreply.github.com>
2021-06-14 07:32:39 +0200

    refactor: Updated wording in print statement of first while loop

--- bikeshare.py
+++ bikeshare.py
@@ -17,16 +17,16 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-    while True: 
-        city = input("Would you like to see data for Chicago, New York City, or Washington?").lower() 
+    while True:
+        city = input("Would you like to see data for Chicago, New York City, or Washington?").lower()
         if city not in CITY_DATA:
-            print("The city name is not available. Please choose a correct city name.")
+            print("The city name you entered is not available. Please choose a correct city name.")
         else:
             break
 
-        
+
     # TO DO: get user input for month (all, january, february, ... , june)
-    while True: 
+    while True:
         month = input("Please choose a month between January and June for which you would like to see the data. If you want to see all months just type 'all'.").lower()
         months = ["january", "february", "march", "april", "may", "june"]
         if month != "all" and month not in months:
@@ -62,28 +62,28 @@ def load_data(city, month, day):
     # INPUT FROM PRACTICE QUESTION #3
     # Load data file into a dataframe
     df = pd.read_csv(CITY_DATA[city])
-    
+
     # This converts the "Start Time" column to datetime
     df["Start Time"] = pd.to_datetime(df["Start Time"])
-    
+
     # Extract month and day from "Start Time" to create new columns
     df["month"] = df["Start Time"].dt.month
     df["day_of_week"] = df["Start Time"].dt.day_name()
-    
+
     # This is supposed to filter by month (if applicable)
     if month != "all":
-        
+
         # Use the index of the months list to get the corresponding int
         months = ["january", "february", "march", "april", "may", "june"]
         month = months.index(month) + 1
-    
+
         # Filter by month to create the new dataframe
         df = df[df["month"] == month]
-    
+
     #Filter by day of week if applicable
     if day != "all":
         # Filter by day of week to create the new dataframe
-        df = df[df["day_of_week"] == day.title()]        
+        df = df[df["day_of_week"] == day.title()]
 
     return df
 
@@ -91,14 +91,14 @@ def load_data(city, month, day):
 def show_raw_data(df):
     """
     Supposed to show subsequent rows of data according to the answer of the user.
-    
+
     Args:
         Pandas DataFrame which includes city data filtered by month and day being returned from the previous load_data() function.
     """
     i = 0
     user_answer = input("Would you like to see the first 5 rows of data? Please enter 'yes' or 'no': ").lower()
     pd.set_option("display.max_columns", None)
-    
+
     while True:
         if user_answer == "no":
             break
@@ -116,7 +116,7 @@ def time_stats(df):
     # TO DO: display the most common month
     common_month = df["month"].mode()[0]
     print("Most common month: ", common_month)
-         
+
     # TO DO: display the most common day of week
     common_day = df["day_of_week"].mode()[0]
     print("Most common day: ", common_day)
@@ -188,10 +188,10 @@ def user_stats(df):
     if "Birth Year" in df:
         earliest_birth_year = int(df["Birth Year"].min())
         print("Earliest year of birth: ", earliest_birth_year)
-        
+
         most_recent_birth_year = int(df["Birth Year"].max())
         print("Most recent year of birth: ", most_recent_birth_year)
-        
+
         most_common_birth_year = int(df["Birth Year"].mode()[0])
         print("Most common year of birth: ", most_common_birth_year)
 
